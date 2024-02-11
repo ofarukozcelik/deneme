6549433e0e5e102a3361f0cd9c9366137926dbce
ofarukozcelik <ofaruko06@gmail.com>
2024-02-11 23:05:55 +0300

    first commit

---
+++ App.tsx
@@ -0,0 +1,10 @@
+import { View, Text, SafeAreaView } from 'react-native'
+import React from 'react'
+
+export default function App() {
+  return (
+    <SafeAreaView>
+      <Text>Taş Kağıt Makas</Text>
+    </SafeAreaView>
+  )
+}
